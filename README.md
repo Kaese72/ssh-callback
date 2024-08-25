ae94882f5dd0a242d0a1b30cb8f7f3d1b0d4943a
Kaese <charliegen@hotmail.se>
2024-08-25 15:53:44 +0200

    minor changes to documentation and deployment file

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ can be done via `generatehostkeys.sh. This will create a secret in the default s
 After that add the keys you want to be able to authenticate to the SSH service to
 a `authorized_keys` file and load it into a secret.
 
-> kubectl --namespace default create secret generic ssh-keys --from-file=./authorized_keys
+> kubectl create configmap ssh-keys --from-file=./authorized_keys
 
 Once this is done, the deployment.yaml file can be deployed. Be wary of the nodeport
 port assignment.
--- generatehostkeys.sh
+++ generatehostkeys.sh
@@ -6,4 +6,4 @@ ssh-keygen -q -t rsa  -f hostkeys.d/ssh_host_rsa_key -N "" -C ""
 ssh-keygen -q -t dsa  -f hostkeys.d/ssh_host_dsa_key -N "" -C ""
 ssh-keygen -q -t ecdsa  -f hostkeys.d/ssh_host_ecdsa_key -N "" -C ""
 ssh-keygen -q -t ed25519  -f hostkeys.d/ssh_host_ed25519_key -N "" -C ""
-kubectl --namespace default create secret generic ssh-host-keys --from-file=hostkeys.d/ssh_host_rsa_key --from-file=hostkeys.d/ssh_host_rsa_key.pub --from-file=hostkeys.d/ssh_host_dsa_key --from-file=hostkeys.d/ssh_host_dsa_key.pub --from-file=hostkeys.d/ssh_host_ecdsa_key --from-file=hostkeys.d/ssh_host_ecdsa_key.pub --from-file=hostkeys.d/ssh_host_ed25519_key --from-file=hostkeys.d/ssh_host_ed25519_key.pub
+kubectl create secret generic ssh-host-keys --from-file=hostkeys.d/ssh_host_rsa_key --from-file=hostkeys.d/ssh_host_rsa_key.pub --from-file=hostkeys.d/ssh_host_dsa_key --from-file=hostkeys.d/ssh_host_dsa_key.pub --from-file=hostkeys.d/ssh_host_ecdsa_key --from-file=hostkeys.d/ssh_host_ecdsa_key.pub --from-file=hostkeys.d/ssh_host_ed25519_key --from-file=hostkeys.d/ssh_host_ed25519_key.pub
